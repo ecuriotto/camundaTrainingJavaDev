94f15550e1972e56953909d530a0424f6bc27451
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2022-01-21 16:41:54 +0200

    ex4

---
+++ src/main/java/com/camunda/training/service/TwitterService.java
@@ -0,0 +1,32 @@
+package com.camunda.training.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+@Service
+
+public class TwitterService {
+
+
+
+  private final Logger LOGGER = LoggerFactory.getLogger(TwitterService.class.getName());
+
+  public long tweet(String content) throws Exception {
+
+    LOGGER.info("Publishing tweet: " + content);
+    AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo",
+        "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+    Twitter twitter = new TwitterFactory().getInstance();
+    twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk",
+        "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+    twitter.setOAuthAccessToken(accessToken);
+    Status status = twitter.updateStatus(content);
+    return status.getId();
+  }
+}
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,24 +1,35 @@
 package com.camunda.training;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import java.util.HashMap;
 import java.util.Map;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import com.camunda.training.service.TwitterService;
 
 
 @Deployment(resources = "twitterDemo.bpmn")
-@ExtendWith(ProcessEngineExtension.class)
+@ExtendWith(ProcessEngineCoverageExtension.class)
 public class ProcessJUnitTest {
 
+  @Mock
+  TwitterService tweetService;
+
   @Test
-  public void testHappyPath() {
+  public void testHappyPath() throws Exception {
+    // Mocks.register("createTweetDelegate", new CreateTweetDelegate(tweetService));
+    when(tweetService.tweet(anyString())).thenReturn(anyLong());
+
     // Create a HashMap to put in variables for the process instance
     Map variables = new HashMap<String, Object>();
     variables.put("approved", true);
+    variables.put("content", "Exercise 4 test - enrico " + System.currentTimeMillis());
     ProcessInstance processInstance =
         runtimeService().startProcessInstanceByKey("twitterProcess", variables);
     assertThat(processInstance).isEnded();
