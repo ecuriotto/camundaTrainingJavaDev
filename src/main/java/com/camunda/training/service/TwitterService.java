de1ac945ea706161343b9b4a675d6d82dcd6601a
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2022-03-15 16:56:05 +0200

    token update

--- src/main/java/com/camunda/training/service/TwitterService.java
+++ src/main/java/com/camunda/training/service/TwitterService.java
@@ -21,8 +21,8 @@ public class TwitterService {
 
       LOGGER.info("Publishing tweet: " + content);
       AccessToken accessToken =
-          new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo",
-              "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+          new AccessToken("220324559-CO8TfUmrcoCrvFHP4TacgToN5hLC8cMw4n2EwmHo",
+              "WvVureFv5TBWTGhESgGe3fqZM7XbGMuyIhxB84zgcoUER");
       Twitter twitter = new TwitterFactory().getInstance();
       twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk",
           "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
